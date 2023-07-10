35b2361a2bc20ee603012569c285aec6e3a47e2f
crispelo <penagoscristian61@gmail.com>
2023-07-10 15:15:52 -0500

    funcionalidad aumentar letra y disminuir letra

--- final/static/js/aumentarFuente.js
+++ final/static/js/aumentarFuente.js
@@ -1,10 +1,23 @@
 
-var fontSize = 1;
+var fontSize = 1; //valor predeterminado
+
+        /*obtiene todos las clases tag y cualquier cosa que contenga despues del la etiqueta body 
+        y hace un recorrido a cada una de las etiquetas hasta que termine el archivo html y por cada elemento suma un 0.01 em*/
         function aumentar() {
-            fontSize += 0.2;
-            document.body.style.fontSize = fontSize + btn+ "em" ;
+            fontSize += 0.01;
+            var elementosDOM = document.querySelectorAll("body *");
+            for (let index = 0; index < elementosDOM.length; index++) {
+               elementosDOM[index].style.fontSize = fontSize + "em" ;
+            }
+            
         }
+
+        /*obtiene todos las clases tag y cualquier cosa que contenga despues del la etiqueta body 
+        y hace un recorrido a cada una de las etiquetas hasta que termine el archivo html y por cada elemento resta un 0.01 em*/
          function disminuir() {
-            fontSize -= 0.2;
-            document.body.style.fontSize = fontSize + "em";
+            fontSize -= 0.01;
+            var  elementosDOM = document.querySelectorAll("body *");
+            for(let index = 0; index < elementosDOM.length; index++) {
+                elementosDOM[index].style.fontSize = fontSize + "em";
+            }
         }
