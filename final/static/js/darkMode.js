dd77334b25aac9a9c19ad0c196c4721968112d05
crispelo <penagoscristian61@gmail.com>
2023-07-10 23:07:55 -0500

    things

--- final/static/js/darkMode.js
+++ final/static/js/darkMode.js
@@ -1,7 +1,7 @@
 function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('dark-mode'); /*cualquier tag que contenga este la cambia*/
 
-    var navLinks = document.querySelectorAll('.nav-link');
+    var navLinks = document.querySelectorAll('.nav-link'); /*hace los cambios del navlink cuando contenga dark-mode clase a un texto blanco de lo contrario lo convierte en negro*/
     for (var i = 0 ; i < navLinks.length; i++) {
         if (document.body.classList.contains('dark-mode')) {
             navLinks[i].classList.remove('text-dark');
