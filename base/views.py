5c0e79c82342ee06905a409d586a0452bc5484c5
Crispelo <penagoscristian61@gmail.com>
2024-02-26 14:55:30 -0500

    logic pass data

--- base/views.py
+++ base/views.py
@@ -24,22 +24,23 @@ def panel_personalizado(request):
     cantidad_usuarios_staff = User.objects.filter(is_staff = 1).count()
 
     data = {
-        #versiones sistemas_operativos_cantidad_activos_e_inactivos
-        "version_s_operativo_id": cantidad_version_so,
-        "version_so_activo": cantidad_version_so_activo,
-        "version_so_inactivo": cantidad_version_so_inactivo,
-        #software and hardware
-        "cantidad_registros_hardware": cantidad_registros_hardware,
-        "cantidad_registros_software": cantidad_registros_software,
+
         #informacion usuarios
         "cantidad_usuarios_registrados": cantidad_usuarios_registrados,
         "cantidad_usuarios_activos":cantidad_usuarios_activos,
         "cantidad_usuarios_inactivos":cantidad_usuarios_inactivos,
-        "cantidad_usuarios_staff":cantidad_usuarios_staff
+        "cantidad_usuarios_staff":cantidad_usuarios_staff,
+        #versiones sistemas_operativos_cantidad_activos_e_inactivos
+        "cantidad_version_so": cantidad_version_so,
+        "cantidad_version_so_activo": cantidad_version_so_activo,
+        "cantidad_version_so_inactivo": cantidad_version_so_inactivo,
+        #hardware and software
+        "cantidad_registros_software": cantidad_registros_software,
+        "cantidad_registros_hardware": cantidad_registros_hardware,
     }
     
-    
+    print(data)
     info_panel = json.dumps(data)
-
+    print(info_panel)
     return render(request, 'panel/panel_admin.html',{'info_panel':info_panel})
     
